1f38262f68ae2b736f1fdf7c3e497cfc839e7d1f
Onur Osman Güler <guleronurosman@gmail.com>
2020-08-20 16:34:59 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
